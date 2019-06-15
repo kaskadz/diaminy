432482fb73bcaf233de535a343854d30e1a9d2d8
Kasper <kasper.kadzielawa@gmail.com>
2019-06-15 20:16:27 +0200

    Map loading works

--- main.cpp
+++ main.cpp
@@ -5,27 +5,24 @@
 #include <algorithm>
 
 class Map {
-public:
-    int const height;
-    int const width;
-    int const maxMoves;
 private:
-    char const **map;
+    char **map;
 
-private:
-    Map(int height, int width, int maxMoves, const char **map)
+    Map(int height, int width, int maxMoves, char **map)
             : height(height), width(width), maxMoves(maxMoves), map(map) {
     }
 
 public:
+    int const height;
+    int const width;
+    int const maxMoves;
+
     ~Map() {
         for (int i = 0; i < height; ++i) {
             delete[] map[i];
         }
         delete[] map;
     };
-
-    static Map *Create(int height, int width, int maxMoves, std::vector<std::string> mapLines);
 
     const char at(int x, int y) {
         if (x < 0 || y < 0 || x >= width || y >= height) {
@@ -36,26 +33,33 @@ public:
     }
 
     void print() {
-        for (int i = height - 1; i >= 0; ++i) {
+        for (int i = height - 1; i >= 0; --i) {
             for (int j = 0; j < width; ++j) {
-                std::cout << at(i, j);
+                std::cout << at(j, i);
             }
+            std::cout << std::endl;
         }
     }
+
+    static Map *CreateFromInputStream(std::istream &stream);
 };
 
-Map *Map::Create(int height, int width, int maxMoves, std::vector<std::string> mapLines) {
-    if (mapLines.size() != height) {
-        throw "Map height does not match read input";
-    }
-    if (std::any_of(mapLines.begin(), mapLines.end(), [width](std::string s) { return s.length() != width; })) {
-        throw "Map width does not match read input";
-    }
+Map *Map::CreateFromInputStream(std::istream &stream) {
+    int height, width, maxMoves;
+    char **map;
 
-    const char **map = new const char *[height];
-    for (int i = height - 1; i >= 0; i++) {
+    stream >> height >> width >> maxMoves;
+    map = new char *[height];
+
+    stream.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::string line;
+    stream >> std::noskipws;
+    for (int i = height - 1; i >= 0; --i) {
         map[i] = new char[width];
-        std::copy(mapLines[i].begin(), mapLines[i].end(), map[i]);
+        for (int j = 0; j < width; ++j) {
+            stream >> map[i][j];
+        }
+        stream.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     }
 
     return new Map(height, width, maxMoves, map);
@@ -66,19 +70,11 @@ Map *ReadMapFromFile(char *filename) {
 
     inputFile.open(filename);
     if (inputFile.is_open()) {
-        int height, width, maxMoves;
-        std::vector<std::string> mapLines;
-
-        inputFile >> height >> width >> maxMoves;
-
-        std::string line;
-        while (std::getline(inputFile, line)) {
-            mapLines.push_back(line);
-        }
+        Map *map = Map::CreateFromInputStream(inputFile);
 
         inputFile.close();
 
-        return Map::Create(height, width, maxMoves, mapLines);
+        return map;
     } else {
         std::cerr << "Unable to open file" << std::endl;
         std::cerr << strerror(errno) << std::endl;
@@ -87,22 +83,16 @@ Map *ReadMapFromFile(char *filename) {
 }
 
 Map *ReadMapFromStdin() {
-    int height, width, maxMoves;
-    std::vector<std::string> mapLines;
-
-    std::cin >> height >> width >> maxMoves;
-
-    std::string line;
-    while (std::getline(std::cin, line)) {
-        mapLines.push_back(line);
-    }
-
-    return Map::Create(height, width, maxMoves, mapLines);
+    return Map::CreateFromInputStream(std::cin);
 }
 
 int main(int argc, char *argv[]) {
-    Map *map = (argc >= 2) ? ReadMapFromFile(argv[1]) : ReadMapFromStdin();
-    map->print();
+    try {
+        Map *map = (argc >= 2) ? ReadMapFromFile(argv[1]) : ReadMapFromStdin();
+        map->print();
+    } catch (const char *e) {
+        printf("[ERROR]: %s\n", e);
+    }
 
     return 0;
 }
