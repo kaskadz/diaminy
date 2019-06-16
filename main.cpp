8424d25c20bc94154412b74bce4d7d1595607dfb
Kasper <kasper.kadzielawa@gmail.com>
2019-06-16 14:03:43 +0200

    Creating movement graph works

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <cstring>
 #include <vector>
 #include <algorithm>
+#include <queue>
 
 enum Entity {
     SHIP = '.',
@@ -13,7 +14,7 @@ enum Entity {
     VOID = ' '
 };
 
-enum Direction {
+enum Direction : int {
     N = 0,
     NE,
     E,
@@ -62,8 +63,9 @@ class Map {
 private:
     char **map;
 
-    Map(int height, int width, int maxMoves, char **map, Position shipPosition)
-            : height(height), width(width), maxMoves(maxMoves), map(map), shipInitialPosition(shipPosition) {
+    Map(int height, int width, int maxMoves, char **map, Position shipPosition, int targetScore)
+            : height(height), width(width), maxMoves(maxMoves), map(map), shipInitialPosition(shipPosition),
+              targetScore(targetScore) {
     }
 
     void set(Position position, Entity e) {
@@ -82,6 +84,7 @@ public:
     int const height;
     int const width;
     int const maxMoves;
+    int const targetScore;
     const Position shipInitialPosition;
 
     ~Map() {
@@ -105,7 +108,7 @@ public:
 
     void print() {
         printf("h: %d w: %d\n", height, width);
-        printf("m: %d\n", maxMoves);
+        printf("m: %d t: %d\n", maxMoves, targetScore);
         printf("s: (%d, %d)\n", shipInitialPosition.x, shipInitialPosition.y);
 
         for (int i = height - 1; i >= 0; --i) {
@@ -138,6 +141,26 @@ public:
         };
     }
 
+    MoveData move(Position initial, int direction) {
+        if (direction < 0 || direction >= 8) {
+            throw "Enum out of range";
+        }
+
+        return move(initial, (Direction) direction);
+    }
+
+    int abs_position(Position position) {
+        return width * position.y + position.x;
+    }
+
+    int abs_positions() {
+        return width * height;
+    }
+
+    Position rel_position(int pos) {
+        return {pos % width, pos / width};
+    }
+
     static Map *CreateFromInputStream(std::istream &stream);
 };
 
@@ -149,6 +172,7 @@ Map *Map::CreateFromInputStream(std::istream &stream) {
     map = new char *[height];
 
     Position shipPosition = Position(-1, -1);
+    int targetScore = 0;
 
     stream.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     std::string line;
@@ -157,7 +181,9 @@ Map *Map::CreateFromInputStream(std::istream &stream) {
         map[i] = new char[width];
         for (int j = 0; j < width; ++j) {
             stream >> map[i][j];
-            if (map[i][j] == SHIP) {
+            if (map[i][j] == DIAX) {
+                targetScore++;
+            } else if (map[i][j] == SHIP) {
                 map[i][j] = VOID;
                 shipPosition = Position(j, i);
             }
@@ -169,7 +195,7 @@ Map *Map::CreateFromInputStream(std::istream &stream) {
         throw "Ship not found";
     }
 
-    return new Map(height, width, maxMoves, map, shipPosition);
+    return new Map(height, width, maxMoves, map, shipPosition, targetScore);
 }
 
 Map *ReadMapFromFile(char *filename) {
@@ -193,13 +219,106 @@ Map *ReadMapFromStdin() {
     return Map::CreateFromInputStream(std::cin);
 }
 
+struct Edge {
+    int weight;
+    int direction;
+    Position to;
+};
+
+class Graph {
+private:
+    Map *map;
+public:
+    std::vector<Edge> *neighbours;
+    int const size;
+
+private:
+    explicit Graph(std::vector<Edge> *vertex, int size, Map *map) : neighbours(vertex), size(size), map(map) {
+    }
+
+public:
+    static Graph *Generate(Map *map) {
+        auto *neighbours = new std::vector<Edge>[map->abs_positions()];
+
+        std::queue<Position> positions;
+        positions.push(map->shipInitialPosition);
+        while (!positions.empty()) {
+            Position pos = positions.front();
+            positions.pop();
+            if (neighbours[map->abs_position(pos)].empty()) {
+                for (int d = 0; d < 8; ++d) {
+                    MoveData md = map->move(pos, d);
+                    if (md.finalPosition.x != pos.x || md.finalPosition.y != pos.y) {
+                        int weight = md.diamondsGathered->size();
+                        neighbours[map->abs_position(pos)].push_back(Edge{weight, d, md.finalPosition});
+                        if (neighbours[map->abs_position(md.finalPosition)].empty()) {
+                            positions.push(md.finalPosition);
+                        }
+                    }
+                    delete md.diamondsGathered;
+                }
+            }
+        }
+
+        return new Graph(neighbours, map->abs_positions(), map);
+    }
+
+    ~Graph() {
+        delete neighbours;
+    }
+
+    void print() {
+        for (int i = 0; i < size; ++i) {
+            if (!neighbours[i].empty()) {
+                Position position = map->rel_position(i);
+                printf("(%d,%d): ", position.x, position.y);
+                for (Edge e : neighbours[i]) {
+                    printf("{(%d,%d), %d, %d} ", e.to.x, e.to.y, e.weight, e.direction);
+                }
+                printf("\n");
+            }
+        }
+    }
+
+    void print_visited_map() {
+        for (int i = map->height - 1; i >= 0; --i) {
+            for (int j = 0; j < map->width; ++j) {
+                auto pos = Position(j, i);
+                if (neighbours[map->abs_position(pos)].empty()) {
+                    std::cout << map->at(pos);
+                } else {
+                    std::cout << 'X';
+                }
+            }
+            std::cout << std::endl;
+        }
+    }
+
+    void print_dot() {
+        printf("digraph diaminy {\n");
+        printf("\trankdir=TOP \n");
+        printf("\tnode [style=filled, shape=circle, color=lightgreen]; \n");
+        for (int i = 0; i < size; ++i) {
+            if (!neighbours[i].empty()) {
+                Position position = map->rel_position(i);
+                for (Edge e : neighbours[i]) {
+                    printf("\t\"(%d,%d)\" -> \"(%d,%d)\" [label=%d];\n",
+                           position.x, position.y, e.to.x, e.to.y, e.weight);
+                }
+            }
+        }
+        printf("\t\"(%d,%d)\" [color=gold]\n", map->shipInitialPosition.x, map->shipInitialPosition.y);
+        printf("}\n");
+    }
+};
+
 int main(int argc, char *argv[]) {
     try {
         Map *map = (argc >= 2) ? ReadMapFromFile(argv[1]) : ReadMapFromStdin();
         map->print();
-        MoveData moveData = map->move(map->shipInitialPosition, NE);
-        printf("(%d,%d) %d", moveData.finalPosition.x, moveData.finalPosition.y, moveData.diamondsGathered->size());
-        delete moveData.diamondsGathered;
+
+        Graph *graph = Graph::Generate(map);
+        graph->print_dot();
 
         delete map;
     } catch (const char *e) {
